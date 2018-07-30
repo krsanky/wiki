c52add1343a11aeaddf6a7a2eab6cfeef4277198
paul wisehart <paul@oldcode.org>
2018-07-30 12:12:08 -0400

    test1.c shows looping through string with strsep (neato)

--- test1.c
+++ test1.c
@@ -2,29 +2,30 @@
 #include <string.h>
 #include <stdlib.h>
 
+void 
+teststrsep(char * str)
+{
+	char	*str2;
+
+	do {
+		str2 = strsep(&str, "/");
+		printf("str2:%s str:%s\n", str2, str);
+	} while (str != NULL);
+}
+
 int
 main(int argc, char **argv)
 {
-/*
-	char          **ap, *argv[10], *inputstring;
-
-	for (ap = argv; ap < &argv[9] &&
-	     (*ap = strsep(&inputstring, " \t")) != NULL;) {
-		if (**ap != '\0')
-			ap++;
-	}
-	*ap = NULL;
-*/
-	char	*string1;
-	char	*orig;
-
 	printf("arg0:%s argc:%d\n", argv[0], argc);
 	if (argc > 1) {
-		printf("arg1:%s\n", argv[1]);
-		string1 = argv[1];
-		orig = strsep(&string1, "/");
-		printf("orig:%s string1:%s\n", orig, string1);
+		teststrsep(argv[1]);
 	}
+
+/* 
+arg0:./a.out argc:2 
+arg1:asd/123/rtyrty/456
+str2:asd string1:123/rtyrty/456
+*/
 
 	return 0;
 }
