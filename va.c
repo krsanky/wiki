40dacb4f5f0d8fbd03d4d2001431015b59d7939b
paul wisehart <paul@oldcode.org>
2019-01-10 17:39:55 +0000

    add missing va_end

--- va.c
+++ va.c
@@ -2,36 +2,6 @@
 #include <stdarg.h>
 
 void
-foo(char *fmt, ...)
-{
-	va_list ap;
-	int d, c;
-	char *s;
-	double f;
-
-	va_start(ap, fmt);
-	while (*fmt)
-	   switch (*fmt++) {
-	   case 's':                       /* string */
-		   s = va_arg(ap, char *);
-		   printf("string %s\n", s);
-		   break;
-	   case 'd':                       /* int */
-		   d = va_arg(ap, int);
-		   printf("int %d\n", d);
-		   break;
-	   case 'c':                       /* char */
-		   c = va_arg(ap, int);    /* promoted */
-		   printf("char %c\n", c);
-		   break;
-	   case 'f':                       /* float */
-		   f = va_arg(ap, double); /* promoted */
-		   printf("float %f\n", f);
-	   }
-	va_end(ap);
-}
-
-void
 f1(int n, ...)
 {
 	char		*v;
@@ -43,6 +13,7 @@ f1(int n, ...)
 		printf("%d %s\n", i, v);
 	}
 
+	va_end(ap);
 }
 
 int
