2e9b99794071e6e239cf9c72636512a462b242d0
paul wisehart <paul@oldcode.org>
2019-01-21 19:07:53 +0000

    long_page.c shows lots of output from printf to cgi works fine...

--- long_page.c
+++ long_page.c
@@ -6,12 +6,21 @@
 #include "myhtml.h"
 #include "util.h"
 
+int		len = 10000;
+
 int
 main()
 {
 	http_headers();
 	myhtml_header();
 	myhtml_breadcrumbs(NULL, NULL, NULL);
+
+	printf("<ul>\n");
+	for (int i=0; i<len; i++) {
+		printf("<li>%d:<a href='/long_page.cgi'>long_page</a></li>\n", i);
+	}
+	printf("</ul>\n");
+
 	myhtml_footer();
 	return EXIT_SUCCESS;
 }
