3c3cd112877bdbed30b91d06857b74e0c1566f03
paul wisehart <paul@oldcode.org>
2019-06-29 18:26:33 +0000

    cool time basics

--- test_time.c
+++ test_time.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int
 main()
 {
+	printf("time\n");
+
+	time_t 		timer;
+	char 		buf      [256];
+	struct tm      *tm_info;
+
+	time(&timer);
+	tm_info = localtime(&timer);
+
+	strftime(buf, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+	printf("%s\n", buf);
+
 	return EXIT_SUCCESS;
 }
